f0bbd88f0445c3db422f3067d2bf63db6d5ef0c7
Paul Trunz <patru@patru.ch>
2011-01-11 04:54:20 +0100

    environment-Task angepasst

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,17 +6,18 @@ set :repository,  "git@git.assembla.com:soft-werker-tt-register.git"
 set :domain, "tt.soft-werker.ch"                           # The URL for your app
 
 set :scm, 'git'
+set :scm_username, "patru"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :branch, "master"
 set :deploy_via, :remote_cache
 set :deploy_to, "/home/p35548r0/app/tt"
 set :rails_env, :production
+set :use_sudo, false
 
 role :web, "soft-werker.ch"                          # Your HTTP server, Apache/etc
 role :app, "soft-werker.ch"                          # This may be the same as your `Web` server
 role :db,  "soft-werker.ch", :primary => true        # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
-set :user, "patru.ch"
-set :use_sudo, false
 
 desc "Set the proper permissions for directories and files on HostingRails accounts"
 set :chmod755, %w(app config db lib public vendor script tmp public/dispatch.cgi public/dispatch.fcgi public/dispatch.rb)
@@ -25,7 +26,6 @@ task :after_deploy do
     "chmod 755 #{current_path}/#{item}"
   end.join(" && "))
 end
-
 
 set :group_writable, false
 
@@ -41,5 +41,5 @@ set :group_writable, false
 #   end
 # end
 task :after_update_code, :roles => :app do
-    run "sed -e \"s/^# ENV/ENV/\" -i #{release_path}/tt/config/environment.rb"
+    run "sed -e \"s/^# ENV/ENV/\" -i #{release_path}/config/environment.rb"
 end
