08f819879d8cadc73f301be71876bb2f2df952ac
Paul Trunz <patru@patru.ch>
2012-07-14 23:27:02 +0200

    added a little more validation to the Email-form

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,5 +1,6 @@
 class Email
   attr_accessor :from, :subject, :text, :errors
+  PATTERN=/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
   def initialize(from = "", subject="", text="")
     self.from=from
@@ -11,8 +12,12 @@ class Email
   def valid?
     if from.blank?
       errors.add_to_base "Wir benötigen deine Email-Adresse um dich kontaktieren zu können!"
-      return false
+    elsif not from =~ PATTERN
+      errors.add_to_base "Das Format deiner Email-Adresse ist nicht korrekt"
     end
-    return true
+    if text.blank?
+      errors.add_to_base "Der Text deiner Meldung ist leer"
+    end
+    errors.size == 0
   end
 end
