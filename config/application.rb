7875c7b30cf93d865f6d45b1e8dea19129dd431f
patru <patru@patru.ch>
2014-02-20 23:24:07 +0100

    get rid of enforce_available_locales warning (by setting it to true)

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module TtRegister
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = true
 
     # JavaScript files you want as :defaults (ttreg.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
