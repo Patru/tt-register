78d4f4b987c889eab419cbf000c85d47f7a29070
patru <patru@soft-werker.ch>
2018-09-24 19:12:10 +0200

    allow nil values for new fields in series

--- app/models/series.rb
+++ app/models/series.rb
@@ -8,8 +8,8 @@ class Series < ActiveRecord::Base
   has_many :waiting_list_series
   validates_numericality_of :min_ranking, :max_ranking
   validate :category_is_valid
-  validates_numericality_of :max_participants
-  validates_numericality_of :non_licensed_start
+  validates_numericality_of :max_participants, allow_nil:true
+  validates_numericality_of :non_licensed_start, allow_nil:true
   attr_accessible :type, :tournament_day_id, :series_name, :long_name, :min_ranking, :max_ranking,
                   :category, :sex, :use_rank, :start_time, :min_elo, :max_elo, :sys_exp_link_de,
                   :sys_exp_link_fr, :sys_exp_link_en, :max_participants
