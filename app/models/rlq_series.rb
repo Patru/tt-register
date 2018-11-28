3ea1694e9b6019ba82e391f848829b537a52ccca
patru <patru@soft-werker.ch>
2018-11-28 08:11:33 +0100

    add option to prohibit the creation of new accounts

--- app/models/rlq_series.rb
+++ app/models/rlq_series.rb
@@ -3,11 +3,9 @@
 class RlqSeries < Series
 
   # does not sound clean, but I lack the time for a decent abstraction TODO
-=begin
   def ranking_of(player)
     player.elo
   end
-=end
 
   def lister
     :ParticipationLister
