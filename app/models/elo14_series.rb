bb380ee164e0e58e6d7dabbe62459ddc6738192f
patru <patru@soft-werker.ch>
2019-11-14 05:17:59 +0100

    adding text keys for elo-series namings

--- app/models/elo14_series.rb
+++ app/models/elo14_series.rb
@@ -6,4 +6,25 @@ class Elo14Series < EloSeries
     :Elo14Lister
   end
 
+  def nav_key
+    :elo_14_nav_name
+  end
+
+  def elo_min_min_max_key
+    :elo_14_min_max
+  end
+
+  def elo_min_key
+    :elo_14_min
+  end
+
+  def elo_max_key
+    :elo_14_max
+  end
+
+  def elo_key
+    :elo_14
+  end
+
+
 end
--- app/models/elo_series.rb
+++ app/models/elo_series.rb
@@ -4,19 +4,39 @@ class EloSeries < Series
   def translated_name
     if min_elo
       if max_elo
-        return I18n.translate :elo_12_min_max, min:min_elo, max:max_elo
+        return I18n.translate elo_min_min_max_key, min:min_elo, max:max_elo
       else
-        return I18n.translate :elo_12_min, min:min_elo
+        return I18n.translate elo_min_key, min:min_elo
       end
     elsif max_elo
-      return I18n.translate :elo_12_max, max:max_elo
+      return I18n.translate elo_max_key, max:max_elo
     else
-      return I18n.translate :elo_12
+      return I18n.translate elo_key
     end
   end
 
   def nav_name
-    @nav_name ||= "#{I18n.t(:elo_nav_name)} #{I18n.localize(tournament_day.day, format: :abr_day)}"
+    @nav_name ||= "#{I18n.t(nav_key)} #{I18n.localize(tournament_day.day, format: :abr_day)}"
+  end
+
+  def nav_key
+    :elo_nav_name
+  end
+
+  def elo_min_min_max_key
+    :elo_12_min_max
+  end
+
+  def elo_min_key
+    :elo_12_min
+  end
+
+  def elo_max_key
+    :elo_12_max
+  end
+
+  def elo_key
+    :elo_12
   end
 
   def may_be_played_by?(player)
